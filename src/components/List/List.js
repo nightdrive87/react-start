081ec1d66ae6891b9752001832fda6084732340f
Gosia <malgorzata.krysztofik@gmail.com>
2020-06-18 23:23:47 +0200

    first components react

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import styles from './List.scss';
+import React from "react";
+import styles from "./List.scss";
 import Hero from "../Hero/Hero";
+import PropTypes from "prop-types";
+import Column from "../Column/Column";
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    children: PropTypes.node,
+  };
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} ImageLink={this.props.image} />
+        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.columns}>
+            <Column title='Animals'></Column>
+            <Column title='Plants'></Column>
+            <Column title='Minerals'></Column>
+        </div>
       </section>
     );
   }
